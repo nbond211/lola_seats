1d0ab1e4c30f3128b191b9ccfc240e0462280e90
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 14:27:01 -0500

    creates initial SideBar component

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./SideBar.css";
+
+class SideBar extends Component {
+
+  render() {
+
+    const { selectedSeatId } = this.props;
+
+    return (
+      <div id="SideBar">
+      </div>
+    );
+  }
+}
+
+export default SideBar;
