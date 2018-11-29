083ed172b861c7b38cc216c0882a31388c6b7d9f
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 10:13:36 -0500

    creates initial Seat component

---
+++ src/components/Seat/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Seat.css";
+
+class Seat extends Component {
+
+  render() {
+
+    return (
+      <div id="Seat">
+        <h1>Seat</h1>
+      </div>
+    );
+  }
+}
+
+export default Seat;
