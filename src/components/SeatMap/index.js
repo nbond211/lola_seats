9bb5643b7f7cff6f44b0044dc9a659b6080e337b
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 10:03:48 -0500

    adds seat data and Cabin components to SeatMap component

--- src/components/SeatMap/index.js
+++ src/components/SeatMap/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import "./SeatMap.css";
+import prepSeatData from "../../utils/PrepSeatData";
+import rawSeatData from "../../data/seats.json";
+import Cabin from "../Cabin";
 
 class SeatMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedSeatId: "" };
+  }
+
+  componentDidMount() {
+    const formattedSeatData = prepSeatData(rawSeatData);
+
+    this.setState({
+      businessData: formattedSeatData["Business"],
+      firstData: formattedSeatData["First"],
+      economyData: formattedSeatData["Economy"]
+    });
+  }
+
   render() {
+    const { businessData, firstData, economyData} = this.state;
+
     return (
       <div id="SeatMap">
-        <h1>Seat Map</h1>
+        <Cabin cabinClass="Business" data={ businessData }/>
+        <Cabin cabinClass="First" data={ firstData }/>
+        <Cabin cabinClass="Economy" data={ economyData }/>
       </div>
     );
   }
