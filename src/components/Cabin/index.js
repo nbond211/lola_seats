dcf000b5dab046f3143115c0befb9d831b71df6e
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 10:02:29 -0500

    creates initial Cabin component

---
+++ src/components/Cabin/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Cabin.css";
+
+class Cabin extends Component {
+
+  render() {
+
+    return (
+      <div id="Cabin">
+        <h1>Cabin</h1>
+      </div>
+    );
+  }
+}
+
+export default Cabin;
