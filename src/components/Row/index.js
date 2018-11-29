c89b41d9fbb0251360222be25e25787fdff5bd13
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 10:12:15 -0500

    creates initial Row component

---
+++ src/components/Row/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Row.css";
+
+class Row extends Component {
+
+  render() {
+
+    return (
+      <div id="Row">
+        <h1>Row</h1>
+      </div>
+    );
+  }
+}
+
+export default Row;
