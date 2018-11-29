994e6f034c010f3903f11d85c5e71b81420c63c8
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 07:14:53 -0500

    creates initial SeatMap component

---
+++ src/components/SeatMap/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SeatMap extends Component {
+  render() {
+    return (
+      <div id="SeatMap">
+        <h1>Seat Map</h1>
+      </div>
+    );
+  }
+}
+
+export default SeatMap;
