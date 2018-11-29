3548c056c0714c730e2cf8bf0193e5f21eea5724
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 11:47:59 -0500

    renders letter labels in Cabin component

--- src/components/Cabin/index.js
+++ src/components/Cabin/index.js
@@ -14,15 +14,30 @@ class Cabin extends Component {
   render() {
     const { cabinClass, seatLetters, rows, selectedSeatId } = this.props;
 
+    const letterLabelComponents = this.state.seatLetterGroupings.map(group => {
+      return (
+        <div className="letterLabelGroup">
+          {group.map(letter => <span className="letterLabel">{letter}</span>)}
+        </div>
+      );
+    });
+
     const rowComponents = Object.entries(rows).map(row => {
       const rowNumber = row[0];
       const seats = row[1];
-      return <Row key={"row" + rowNumber} rowNumber={ rowNumber } seats={ seats } seatLetters={ seatLetters } selectedSeatId={ selectedSeatId } />;
+      return <Row
+        key={"row" + rowNumber}
+        rowNumber={ rowNumber }
+        seats={ seats }
+        seatLetters={ seatLetters }
+        selectedSeatId={ selectedSeatId }
+      />;
     });
 
     return (
       <div id="Cabin">
         <p>{cabinClass}</p>
+        <div className="letterLabelRow">{letterLabelComponents}</div>
         { rowComponents }
       </div>
     );
