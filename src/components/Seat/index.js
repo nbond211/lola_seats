fe09d514073756a9aeb70aef5de270584141272a
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 17:58:45 -0500

    adds logic to change seat appearance based on state

--- src/components/Seat/index.js
+++ src/components/Seat/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import "./Seat.css";
+import occupiedIcon from "../../assets/occupied-icon.png";
 
 class Seat extends Component {
 
   render() {
-    const { id, isOccupied, isPremium, isOverWing, selectedSeatId, selectSeat } = this.props;
+    const { id, isOccupied, selectedSeatId, selectSeat } = this.props;
+    const isSelected = id === selectedSeatId;
+
+    let seatClassName = "available";
+    let onClick = () => selectSeat(id);
+
+    if (isSelected) {
+      seatClassName = "selected";
+    }
+    else if (isOccupied) {
+      seatClassName = "occupied";
+      onClick = () => false;
+    }
 
     return (
-      <div className="seat" onClick={() => selectSeat(id)}>
+      <div className={`seat ${seatClassName}`} onClick={onClick}>
+        {isOccupied && <img className="occupiedIcon" src={occupiedIcon} alt="occupied"/>}
       </div>
     );
   }
