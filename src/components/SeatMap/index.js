5651f490efa7234c375e35b2713190ab4ebb2233
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 10:36:04 -0500

    adds isDataLoaded to state of SeatMap component and splits seatData Cabin prop into separate seatLetters and rows props

--- src/components/SeatMap/index.js
+++ src/components/SeatMap/index.js
@@ -7,13 +7,14 @@ import Cabin from "../Cabin";
 class SeatMap extends Component {
   constructor(props) {
     super(props);
-    this.state = { selectedSeatId: "" };
+    this.state = { isDateLoaded: false, selectedSeatId: "" };
   }
 
   componentDidMount() {
     const formattedSeatData = prepSeatData(rawSeatData);
 
     this.setState({
+      isDataLoaded: true,
       businessData: formattedSeatData["Business"],
       firstData: formattedSeatData["First"],
       economyData: formattedSeatData["Economy"]
@@ -21,13 +22,37 @@ class SeatMap extends Component {
   }
 
   render() {
-    const { businessData, firstData, economyData, selectedSeatId} = this.state;
+    const { isDataLoaded, businessData, firstData, economyData, selectedSeatId} = this.state;
 
     return (
       <div id="SeatMap">
-        <Cabin cabinClass="Business" seatData={ businessData } selectedSeatId={ selectedSeatId }/>
-        <Cabin cabinClass="First" seatData={ firstData } selectedSeatId={ selectedSeatId }/>
-        <Cabin cabinClass="Economy" seatData={ economyData } selectedSeatId={ selectedSeatId }/>
+        {isDataLoaded &&
+          [
+            <Cabin
+              key="Business"
+              cabinClass="Business"
+              seatLetters={ businessData.seatLetters }
+              rows={ businessData.rows }
+              selectedSeatId={ selectedSeatId }
+            />,
+
+            <Cabin
+              key="First"
+              cabinClass="First"
+              seatLetters={ firstData.seatLetters }
+              rows={ firstData.rows }
+              selectedSeatId={ selectedSeatId }
+            />,
+
+            <Cabin
+              key="Economy"
+              cabinClass="Economy"
+              seatLetters={ economyData.seatLetters }
+              rows={ economyData.rows }
+              selectedSeatId={ selectedSeatId }
+            />
+          ]
+        }
       </div>
     );
   }
