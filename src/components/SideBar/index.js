c8b69966b7bb75dbb9bd9632a8e2e03961621878
Nick Bond <bond.n@husky.neu.edu>
2018-11-29 15:40:38 -0500

    adds logic to display selected seat to SideBar component

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -6,9 +6,31 @@ class SideBar extends Component {
   render() {
 
     const { allSeats, selectedSeatId } = this.props;
+    const selectedSeat = allSeats[selectedSeatId];
+
+    let selectedSeatSection;
+    if (selectedSeat) {
+      selectedSeatSection = (
+        <div className="selectedSeatSection">
+          <p>Selected Seat:</p>
+          <h4>Seat {selectedSeatId}</h4>
+          <p>{selectedSeat.class}</p>
+        </div>
+      );
+    }
+    else {
+      selectedSeatSection = (
+        <div className="selectedSeatSection">
+          <h4>No Seat Selected</h4>
+        </div>
+      );
+    }
 
     return (
       <div id="SideBar">
+        <div className="SidebarContent">
+          {selectedSeatSection}
+        </div>
       </div>
     );
   }
